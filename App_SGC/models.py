3ebc618858f6d80f8c6f3e7a3ec6789032ca7546
PersicoJA <167255401+PersicoJA@users.noreply.github.com>
2024-04-20 17:44:51 -0300

    Verificar login finalizado
    Verificar login finalizado

--- App_SGC/models.py
+++ App_SGC/models.py
@@ -1,19 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Usuario(models.Model):
+# class Usuario(models.Model):
+#     username = models.CharField(primary_key=True, max_length=30, null=False, blank=False)
+#     password = models.CharField(max_length=10, null=False, blank=False)
+#     cpf_usuario = models.IntegerField(null=True, blank=True)
+#     nivel = models.IntegerField(max_length=2, null=True, blank=True)
+#     condominio_numero = models.IntegerField(null=False, blank=False)
+
+# class Condomino(models.Model):
+#     cpf_condomino = models.IntegerField(verbose_name="CPF Condômino *", primary_key=True, null=False, blank=False) 
+#     nome_condomino = models.CharField(verbose_name="Nome Condômino *",max_length=40, null=False, blank=False)
+#     bloco = models.CharField(verbose_name="Bloco *",max_length=2, null=False, blank=False)
+#     apartamento = models.CharField(verbose_name="Apartamento *",max_length=4, null=False, blank=False)
+#     telefone = models.CharField(verbose_name="Telefone",max_length=9, null=True, blank=True)
+#     celular = models.CharField(verbose_name="Celular",max_length=11, null=True, blank=True)
+#     email = models.CharField(verbose_name="Email",max_length=40, null=True, blank=True)
+#     data_aquisicao_imovel = models.DateField(verbose_name="Data aquisição imóvel *", null=False, blank=False)
+#     Condominio_numero = models.IntegerField(null=False, blank=False)
+
+class CustomUser(AbstractUser):
     username = models.CharField(primary_key=True, max_length=30, null=False, blank=False)
-    password = models.CharField(max_length=10, null=False, blank=False)
+    password = models.CharField(max_length=100, null=False, blank=False)
     cpf_usuario = models.IntegerField(null=True, blank=True)
-    nivel = models.CharField(max_length=2, null=True, blank=True)
-    condominio_numero = models.IntegerField(null=False, blank=False)
-
-class Condomino(models.Model):
-    cpf_condomino = models.IntegerField(verbose_name="CPF Condômino *", primary_key=True, null=False, blank=False) 
-    nome_condomino = models.CharField(verbose_name="Nome Condômino *",max_length=40, null=False, blank=False)
-    bloco = models.CharField(verbose_name="Bloco *",max_length=2, null=False, blank=False)
-    apartamento = models.CharField(verbose_name="Apartamento *",max_length=4, null=False, blank=False)
-    telefone = models.CharField(verbose_name="Telefone",max_length=9, null=True, blank=True)
-    celular = models.CharField(verbose_name="Celular",max_length=11, null=True, blank=True)
-    email = models.CharField(verbose_name="Email",max_length=40, null=True, blank=True)
-    data_aquisicao_imovel = models.DateField(verbose_name="Data aquisição imóvel *", null=False, blank=False)
-    Condominio_numero = models.IntegerField(null=False, blank=False)
+    nivel = models.IntegerField(max_length=2, null=True, blank=True)
+    condominio_numero = models.IntegerField(null=True, blank=True)
+    class Meta:
+        db_table='usuario'
