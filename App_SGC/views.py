e0b0e8baef8f37c328791897671932aca141ec5f
PersicoJA <167255401+PersicoJA@users.noreply.github.com>
2024-04-20 17:44:51 -0300

    Verificar login finalizado
    Verificar login finalizado

--- App_SGC/views.py
+++ App_SGC/views.py
@@ -1,59 +1,36 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from .models import Usuario
+from django.contrib.auth import authenticate, login
+# from .models import Usuario 
 from django.contrib.auth.hashers import make_password
+from .models import CustomUser
 
-def login(request):
+def exibirLogin(request):
     return render(request,'login/login.html')
 
-def register(request):
-    return render(request, 'login/register.html')
-
-def realizaRegistro(request):
-    if request.method == 'POST':
-        username = request.POST['usuario']
-        password = request.POST['senha']
-        cpf = request.POST['cpf']
-        nivel = request.POST['nivel']
-        condominio = request.POST['condominio']
-    
-        user = User.objects.create_user(username, password)
-        user.cpf = cpf
-        user.nivel = nivel
-        user.condominio = condominio
-        user.save()
-        
-        return redirect('login')
-    
-    return render(request, "login/register.html")
-#
-def usuarios(request):
-# Busca as informações digitadas na tela
-    novo_usuario = Usuario()
-    novo_usuario.usuario = request.POST.get('usuario')
-    novo_usuario.senha = request.POST.get('senha')
-    novo_usuario.cpf_usuario = 22760369153
-    novo_usuario.nivel = 1
-
-def verificaLogin(request):
+def verificarLogin(request):
 # verifica se as informações digitadas no login conferem com as informações na tabela usuários
     if request.method == 'POST':
         usuario = request.POST['usuario']
         senha = request.POST['senha']
+       
+        print('Antes do authenti... ***********************************')
         user = authenticate(username=usuario,password=senha)
-
-        print(user)
-        
-        
-        
 
         print(usuario,senha)
 
         if user is not None:
             print('Usuário Válido *************************************')
+            login(request, user)
+            return render(request, 'login/home.html')
         else:
             print('Usuário Inválido ***********************************')
+            return redirect('login')
             
 
+def inserirUsuario(request):
+    if request.method == 'POST':
+        usuario = request.POST['usuario']
+        senha = request.POST['senha']
+        user=CustomUser.objects.create_user(username='teste1', password='1234')
+        user.save()
--- Projeto_SGC/urls.py
+++ Projeto_SGC/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from App_SGC import views
 
 urlpatterns = [
-    path('',views.login,name='login'),
-    path('register',views.register,name='register'),
-    path('signin',views.verificaLogin,name='verificaLogin'),
-    path('signup',views.realizaRegistro,name='realizaRegistro'),
     path('admin/', admin.site.urls),
+    path('',views.exibirLogin,name='exibirLogin'),
+#    path('register',views.register,name='register'),
+    path('signin',views.verificarLogin,name='verificarLogin'),
+    path('signup',views.inserirUsuario,name='inserirUsuario'),
+   
 #   path('usuarios/',views.usuarios,name='listagem_usuarios')
 ]
 
