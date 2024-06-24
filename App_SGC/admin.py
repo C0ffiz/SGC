c5a0cf89127534f060554dd131e2ec8819185806
PersicoJA <167255401+PersicoJA@users.noreply.github.com>
2024-06-24 19:02:50 -0300

    latest

--- App_SGC/admin.py
+++ App_SGC/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomCondomino, CustomUser
+from .models import CustomCondomino, CustomUser, CustomCondominio
 # from .models import Usuario
 
 # # Register your models here.
@@ -8,3 +8,4 @@ from .models import CustomCondomino, CustomUser
 
 admin.site.register(CustomUser)
 admin.site.register(CustomCondomino)
+admin.site.register(CustomCondominio)
