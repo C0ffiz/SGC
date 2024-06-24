1e3c6f26e29714ef6c860026003e2497b21b84c1
PersicoJA <167255401+PersicoJA@users.noreply.github.com>
2024-06-24 19:02:50 -0300

    latest

--- App_SGC/views.py
+++ App_SGC/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .models import CustomUser,CustomCondomino
+from .models import CustomUser,CustomCondominio,CustomCondomino,CustomCondominio,CustomMorador
 from django.contrib import messages
 from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
+from django import forms
 
 
 #-----------------------Views Login
@@ -40,26 +41,51 @@ def exibirHome(request):
     return render(request,'login/home.html')
 
 
-
 #-----------------------Views Usuário
 
 # Tela Usuarios
 class UsuariosListViews(ListView):
     model = CustomUser
     context_object_name = 'usuarios_list'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()
+        return context
 
 # Tela Cadastro De Usuarios
 class UsuariosCreateViews(CreateView):
     model = CustomUser
     template_name = 'usuarios_create.html'
-    fields = ["username", "password", "cpf_usuario", "nivel", "condominio_numero"]
+    fields = ["username", "password", "cpf_usuario", "nivel", "n_condominio"]
     success_url = reverse_lazy("usuarios_list")
+    widgets = {
+        'n_condominio': forms.Select(attrs={'class': 'form-control'}),
+    }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()
+        return context
+    
+    
     
     def form_valid(self, form):
         # Strip mask from CPF
         cpf_usuario = form.cleaned_data['cpf_usuario'].replace(".", "").replace("-", "")
         form.instance.cpf_usuario = cpf_usuario
 
+        # Get the selected condominium instance
+        condominio_instance = form.cleaned_data['n_condominio']
+
+        # Assign the primary key of the selected condominium to the field
+        form.instance.n_condominio_id = condominio_instance.n_condominio
+
+        # Check if the selected condominium number exists
+        if not CustomCondominio.objects.filter(n_condominio=form.instance.n_condominio_id).exists():
+            messages.error(self.request, 'Número de condomínio inválido.')
+            return self.form_invalid(form)
+        
         # Verifica se o nome de usuário já existe
         username = form.cleaned_data['username']
         if CustomUser.objects.filter(username=username).exists():
@@ -73,26 +99,47 @@ class UsuariosCreateViews(CreateView):
         if 'username' in form.errors:
             form.errors['username'] = ['Nome de usuário já existe. Escolha outro nome.']
         return super().form_invalid(form)
+    
 
 # Tela Alteração De Usuarios
 class UsuariosUpdateViews(UpdateView):
     model = CustomUser
     context_object_name = 'usuarios_list'
-    fields = ["username", "password", "cpf_usuario", "nivel", "condominio_numero"]
-    success_url = reverse_lazy("usuarios_list")
+    fields = ["username", "password", "cpf_usuario", "nivel", "n_condominio"]
+    success_url = reverse_lazy("usuarios_list") 
+    widgets = {
+        'n_condominio': forms.Select(attrs={'class': 'form-control'}),
+    }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()
+        return context
     
     def form_valid(self, form):
         # Strip mask from CPF
         cpf_usuario = form.cleaned_data['cpf_usuario'].replace(".", "").replace("-", "")
         form.instance.cpf_usuario = cpf_usuario
+        
+        # Get the selected condominium instance
+        condominio_instance = form.cleaned_data['n_condominio']
+
+        # Assign the primary key of the selected condominium to the field
+        form.instance.n_condominio_id = condominio_instance.n_condominio
+
+        # Check if the selected condominium number exists
+        if not CustomCondominio.objects.filter(n_condominio=form.instance.n_condominio_id).exists():
+            messages.error(self.request, 'Número de condomínio inválido.')
+            return self.form_invalid(form)
+        
         return super().form_valid(form)
+    
 
 # Tela Exclusão De Usuarios
 class UsuariosDeleteViews(DeleteView):
     model = CustomUser
     success_url = reverse_lazy("usuarios_list")
-
-
+    
 
 #-----------------------Views Condômino
 
@@ -101,21 +148,239 @@ class CondominosListViews(ListView):
     model = CustomCondomino
     context_object_name = 'condominos_list'
 
+
 # Tela Cadastro De Condominos
 class CondominosCreateViews(CreateView):
     model = CustomCondomino
-    fields = ["cpf_condomino", "nome_condomino", "bloco", "apartamento", "telefone_condomino", "celular_condomino", "email_condomino", "data_aquisicao_imovel", "Condominio_numero"]
+    fields = ["cpf_condomino", "nome_condomino", "bloco", "apartamento", "telefone_condomino", "celular_condomino", "email_condomino", "data_aquisicao_imovel", "data_nascimento_condomino", "n_condominio"]
     data= print("data : ")
     success_url = reverse_lazy("condominos_list")
+    widgets = {
+        'n_condominio': forms.Select(attrs={'class': 'form-control'}),
+    }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()
+        return context       
+    
+    def form_valid(self, form):
+        # Strip mask from CPF
+        cpf_condomino = str(form.cleaned_data['cpf_condomino']).replace(".", "").replace("-", "")
+        form.instance.cpf_usuario = cpf_condomino   
+     
+        # Get the selected condominio instance
+        condominio_instance = form.cleaned_data['n_condominio']
+
+        # Assign the primary key of the selected condominium to the field
+        form.instance.n_condominio_id = condominio_instance.n_condominio
+
+        # Check if the selected condominium number exists
+        if not CustomCondominio.objects.filter(n_condominio=form.instance.n_condominio_id).exists():
+            messages.error(self.request, 'Número de condomínio inválido.')
+            return self.form_invalid(form)  
+       
+        # Verifica se o condômino já está cadastrado
+        cpf_condomino = str(form.cleaned_data['cpf_condomino'])
+        if CustomCondomino.objects.filter(cpf_condomino=cpf_condomino).exists():
+            messages.error(self.request, 'Condômino já cadastrado')
+            return self.form_invalid(form)
+
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        # Customize error message for existing username
+        if 'cpf_condomino' in form.errors:
+            form.errors['cpf_condomino'] = ['Condômino já cadastrado']
+        return super().form_invalid(form)   
+    
 
 # Tela Alteração De Condominos
 class CondominosUpdateViews(UpdateView):
     model = CustomCondomino
-    fields = ["cpf_condomino", "nome_condomino", "bloco", "apartamento", "telefone_condomino", "celular_condomino", "email_condomino", "data_aquisicao_imovel", "Condominio_numero"]
+    context_object_name = 'condominos_list'    
+    fields = ["cpf_condomino", "nome_condomino", "data_nascimento_condomino", "bloco", "apartamento", "telefone_condomino", "celular_condomino", "email_condomino", "data_aquisicao_imovel", "n_condominio"]
     success_url = reverse_lazy("condominos_list")
+    widgets = {
+        'n_condominio': forms.Select(attrs={'class': 'form-control'}),
+    }
+   
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()        
+        return context
+    
+    def form_valid(self, form):
+        # Strip mask from CPF
+        cpf_condomino = str(form.cleaned_data['cpf_condomino']).replace(".", "").replace("-", "")
+        form.instance.cpf_condomino = cpf_condomino
+        
+        # Get the selected condominium instance
+        condominio_instance = form.cleaned_data['n_condominio']
 
+        # Assign the primary key of the selected condominium to the field
+        form.instance.n_condominio_id = condominio_instance.n_condominio
+
+        # Check if the selected condominium number exists
+        if not CustomCondominio.objects.filter(n_condominio=form.instance.n_condominio_id).exists():
+            messages.error(self.request, 'Número de condomínio inválido.')
+            return self.form_invalid(form)
+        
+        return super().form_valid(form)         
+    
 # Tela Exclusão De Condominos
 class CondominosDeleteViews(DeleteView):
     model = CustomCondomino
     success_url = reverse_lazy("condominos_list")
 
+
+#-----------------------Views Condomínios
+
+# Tela Condomínios
+class CondominiosListViews(ListView):
+    model = CustomCondominio
+    context_object_name = 'condominios_list'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()
+        return context
+
+
+# Tela Cadastro De Condomínios
+class CondominiosCreateViews(CreateView):
+    model = CustomCondominio
+    template_name = 'condominios_create.html'
+    fields = ["n_condominio", "nome_condominio"]
+    success_url = reverse_lazy("condominios_list")
+    # widgets = {
+    #     'n_condominio': forms.Select(attrs={'class': 'form-control'}),
+    # }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()
+        return context    
+    
+    def form_invalid(self, form):
+        # Customize error message for existing username
+        if 'username' in form.errors:
+            form.errors['username'] = ['Número do Condomínio já existe. Defina outro número.']
+        return super().form_invalid(form)
+    
+
+# Tela Alteração De Condomínios
+class CondominiosUpdateViews(UpdateView):
+    model = CustomCondominio
+    context_object_name = 'condominios_list'
+    fields = ["n_condominio", "nome_condominio"]
+    success_url = reverse_lazy("condominios_list") 
+    # widgets = {
+    #     'n_condominio': forms.Select(attrs={'class': 'form-control'}),
+    # }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominios'] = CustomCondominio.objects.all()
+        return context      
+
+# Tela Exclusão De Condomínios
+class CondominiosDeleteViews(DeleteView):
+    model = CustomCondominio
+    success_url = reverse_lazy("condominios_list")
+    
+
+#-----------------------Views Moradores
+
+# Tela Moradores
+class MoradoresListViews(ListView):
+    model = CustomMorador
+    context_object_name = 'moradores_list'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['moradores'] = CustomMorador.objects.all()
+        return context
+
+# Tela Cadastro De Moradores
+class MoradoresCreateViews(CreateView):
+    model = CustomMorador
+    template_name = 'moradores_create.html'
+    fields = ["cpf_condomino", "cpf_morador", "nome_morador", "data_nascimento_morador", "celular_morador", "email_morador", "n_condominio"]
+    success_url = reverse_lazy("moradores_list")
+    widgets = {
+        'cpf_condomino': forms.Select(attrs={'class': 'form-control'}),
+    }
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['condominos'] = CustomCondomino.objects.all()
+        return context      
+    
+    def form_valid(self, form):
+        # Strip mask from CPF
+        cpf_condomino = form.cleaned_data['cpf_condomino'].replace(".", "").replace("-", "")
+        form.instance.cpf_condomino = cpf_condomino
+
+       
+        # Assign the primary key of the selected condominium to the field
+        form.instance.n_condominio_id = condominio_instance.n_condominio
+
+        # Check if the selected condominium number exists
+        if not CustomCondominio.objects.filter(n_condominio=form.instance.n_condominio_id).exists():
+            messages.error(self.request, 'Número de condomínio inválido.')
+            return self.form_invalid(form)
+        
+        # Verifica se o cpf do morador já existe
+        cpf_morador = form.cleaned_data['cpf_morador']
+        if CustomUser.objects.filter(cpf_morador=cpf_morador).exists():
+            messages.error(self.request, 'Morador já cadastrado')
+            return self.form_invalid(form)
+
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        # Customize error message for existing username
+        if 'username' in form.errors:
+            form.errors['username'] = ['Morador já cadastrado']
+        return super().form_invalid(form)
+    
+
+# Tela Alteração De Moradores
+class MoradoresUpdateViews(UpdateView):
+    model = CustomMorador
+    # context_object_name = 'usuarios_list'
+    # fields = ["username", "password", "cpf_usuario", "nivel", "n_condominio"]
+    # success_url = reverse_lazy("usuarios_list") 
+    # widgets = {
+    #     'n_condominio': forms.Select(attrs={'class': 'form-control'}),
+    # }
+    
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['condominios'] = CustomCondominio.objects.all()
+#         return context
+    
+    # def form_valid(self, form):
+        # Strip mask from CPF
+        # cpf_usuario = form.cleaned_data['cpf_usuario'].replace(".", "").replace("-", "")
+        # form.instance.cpf_usuario = cpf_usuario
+        
+        # Get the selected condominium instance
+        # condominio_instance = form.cleaned_data['n_condominio']
+
+        # Assign the primary key of the selected condominium to the field
+        # form.instance.n_condominio_id = condominio_instance.n_condominio
+
+        # Check if the selected condominium number exists
+        # if not CustomCondominio.objects.filter(n_condominio=form.instance.n_condominio_id).exists():
+        #     messages.error(self.request, 'Número de condomínio inválido.')
+        #     return self.form_invalid(form)
+        
+        # return super().form_valid(form)
+    
+
+# Tela Exclusão De Usuarios
+class MoradoresDeleteViews(DeleteView):
+    model = CustomMorador
+    success_url = reverse_lazy("moradores_list")
