e86c99c6981acc0aa06253277726de73cfd1f4ef
C0ffiz <93149828+C0ffiz@users.noreply.github.com>
2024-07-01 21:17:05 -0300

    .
    .

--- Projeto_SGC/Projeto_SGC/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from App_SGC import views
-
-urlpatterns = [
-    path('',views.login,name='login'),
-#   path('usuarios/',views.usuarios,name='listagem_usuarios')
-]
-
